864e2a268540dc27e2ca4fa521fc0cf898ce473c
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-03-02 19:04:46 +0530

    added scroll to top button on home page

--- src/app/home1/home1.component.ts
+++ src/app/home1/home1.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener, ElementRef } from '@angular/core';
 @Component({
   selector: 'app-home1',
   templateUrl: './home1.component.html',
   styleUrls: ['./home1.component.scss']
 })
 export class Home1Component {
+  isShow: boolean | undefined;
+  topPosToStartShowing = 100;
 
+  @HostListener('window:scroll')
+  checkScroll() {
+      
+    // window의 scroll top
+    // Both window.pageYOffset and document.documentElement.scrollTop returns the same result in all the cases. window.pageYOffset is not supported below IE 9.
+
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    console.log('[scroll]', scrollPosition);
+    
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+  // TODO: Cross browsing
+  gotoTop() {
+    window.scroll({ 
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth' 
+    });
+  }
 }

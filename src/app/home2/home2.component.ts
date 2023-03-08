edb2415c24a3e60b91cb735ae518829ce7cecfbb
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-03-08 16:46:57 +0530

    scroll to top button added

--- src/app/home2/home2.component.ts
+++ src/app/home2/home2.component.ts
@@ -6,8 +6,10 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./home2.component.scss']
 })
 export class Home2Component {
+  isShow: any;
+  topPosToStartShowing = 100;
   defaultClassNavbarScrollFlag:boolean =false;
-  
+
   @HostListener('window:scroll', ['$event'])
 
   onWindowScroll() {
@@ -20,5 +22,22 @@ export class Home2Component {
         element.classList.remove('navbar-scroll');
       }
     }
+  }
+  checkScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+  // TODO: Cross browsing
+  gotoTop() {
+    window.scroll({ 
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth' 
+    });
   }
 }

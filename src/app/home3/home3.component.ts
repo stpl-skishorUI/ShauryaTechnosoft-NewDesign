bbfbe8f1e417934a5ed5ff8ec0b2b90e843fcd38
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-03-09 19:13:07 +0530

    why choose us section added

--- src/app/home3/home3.component.ts
+++ src/app/home3/home3.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home3',
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./home3.component.scss']
 })
 export class Home3Component {
+  isShow: boolean | undefined;
+  topPosToStartShowing = 100;
+  defaultClassNavbarScrollFlag:boolean =false;
 
+  @HostListener('window:scroll', ['$event'])
+
+  onWindowScroll() {
+    
+    if (!this.defaultClassNavbarScrollFlag) {
+      let element = document.querySelector('.navbar') as HTMLElement;
+      if (window.pageYOffset > element.clientHeight) {
+        element.classList.add('navbar-scroll');
+      } else {
+        element.classList.remove('navbar-scroll');
+      }
+    }
+  }
+  checkScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    console.log('[scroll]', scrollPosition);
+    
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+  // TODO: Cross browsing
+  gotoTop() {
+    window.scroll({ 
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth' 
+    });
+  }
 }
